301a8f21f6078660c43cfca395ed3dcc33129fef
mark <markus@zimmball.de>
2023-06-08 19:35:59 +0200

    refractor

---
+++ KMeans.py
@@ -0,0 +1,64 @@
+import numpy as np
+from scipy.spatial.distance import cdist
+
+
+class KMeans:
+    def __init__(self, n_clusters, max_iter=100, atol=.1, method='lyod', **kwargs):
+        self.n_clusters = n_clusters
+        self.max_iter = max_iter
+        self.atol = atol
+        self.method = method
+        self.centroids = None
+        self.centroids_history = []
+        self.distance_computations = 0
+        for kw, kwarg in kwargs.items():
+            setattr(self, kw, kwarg)
+
+    def fit(self, X):
+        if self.method == 'lyod':
+            self.centroids = self._initialize_centroids(X)
+            
+            for _ in range(self.max_iter):
+                self.centroids_history.append(self.centroids)
+                labels = self._assign_labels(X, self.centroids)
+                new_centroids = self._update_centroids(X, labels)
+
+                if np.allclose(new_centroids, self.centroids, atol=self.atol):
+                    break
+
+                self.centroids = new_centroids
+        elif self.method == 'lsh':
+            pass
+        elif self.method == 'coresets':
+            pass     
+
+    def predict(self, X):
+        if self.method == 'lyod':
+            distances = cdist(X, self.centroids)
+            labels = np.argmin(distances, axis=1)
+        elif self.method == 'lsh':
+            pass
+        elif self.method == 'coresets':
+            pass      
+        return labels
+
+    def _initialize_centroids(self, X):
+        random_indices = np.random.choice(range(X.shape[0]), size=self.n_clusters, replace=False)
+        centroids = X[random_indices]
+        return centroids
+
+    def _assign_labels(self, X, centroids):
+        distances = cdist(X, centroids)
+        self.distance_computations += len(X) * self.n_clusters
+        labels = np.argmin(distances, axis=1)
+        return labels
+
+    def _update_centroids(self, X, labels):
+        unique_labels = np.unique(labels)
+        centroids = np.zeros((self.n_clusters, X.shape[1]))
+
+        for i, label in enumerate(unique_labels):
+            cluster_points = X[labels == label]
+            centroids[i] = np.mean(cluster_points, axis=0)
+
+        return centroids
